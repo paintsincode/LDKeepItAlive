6dfa7140ac6752b9fcea03b0e9f3565a50cde6eb
Scott <scott.dunbar@yoyogames.com>
2020-04-18 11:07:35 +0100

    Added a quick script for testing

---
+++ LD46KIA/Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is like a Create Event
+        Debug.Log("It's Alive! It's ALIVE! -Manic Laugh-");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This is like the Step Event
+        
+    }
+}
